27332dbe9ca6c14a7166b1245cf03d591678b25b
Jesse Charbneau <jessecharbneau@gmail.com>
2024-06-17 19:35:55 -0400

    may not be necessary since we're including it previously, keeping it for now

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+
+import { Chart, registerables } from 'chart.js';
+import 'chartjs-adapter-date-fns';
+
+// Register Chart.js components and adapters globally
+Chart.register(...registerables);
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
