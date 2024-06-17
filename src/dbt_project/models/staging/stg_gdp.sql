cc09d090fe0685b72e696744436d8ace1bdd65a8
Jesse Charbneau <jessecharbneau@gmail.com>
2024-06-17 19:26:32 -0400

    added growth rate calc

--- src/dbt_project/models/staging/stg_gdp.sql
+++ src/dbt_project/models/staging/stg_gdp.sql
@@ -11,5 +11,17 @@ cleaned as (
     from raw
     where date is not null
       and value is not null
+),
+growth_calculation as (
+    select
+        date,
+        gdp_value,
+        lag(gdp_value) over (order by date) as prev_gdp_value
+    from cleaned
 )
-select * from cleaned
+select
+    date,
+    gdp_value,
+    (gdp_value - prev_gdp_value) / prev_gdp_value as gdp_growth_rate
+from growth_calculation
+where prev_gdp_value is not null
